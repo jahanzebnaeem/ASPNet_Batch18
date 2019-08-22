1eae6a3c9dd8ff2507ea40984b152b1f7a43b94f
jahanzebnaeem <jahanzeb.naeem@hotmail.com>
2019-08-22 16:58:48 +0500

    Lecture - 1
    Console app demo to revise code concepts.

---
+++ Lecture1/DemoConsoleApp/DemoConsoleApp/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Iteration - 1
+            //string inPutVal = Console.ReadLine();
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    if (inPutVal.ToLower() == "jahanzeb")
+            //    {
+            //        Console.WriteLine(inPutVal + " has taught ASP.NET Batchs - " + i);
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine(i + " - Hello ASP.NET Batch-18");
+            //    }
+            //}
+
+            // Iteration - 2
+            //int inPutVal = Convert.ToInt32(Console.ReadLine());
+
+            //if (inPutVal % 2 == 0)
+            //    Console.WriteLine("Number is Even");
+            //else
+            //    Console.WriteLine("Number is Odd");
+
+            // Iteration - 3
+            int inPutVal = Convert.ToInt32(Console.ReadLine());
+            int inPutVal2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("The sum of your input values = " + (inPutVal + inPutVal2));
+        }
+    }
+}
