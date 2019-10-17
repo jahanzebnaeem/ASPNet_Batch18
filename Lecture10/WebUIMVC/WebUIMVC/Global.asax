4df08dcb573bd2c785aeea81bbcb49e94074f227
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2019-10-17 15:20:42 +0500

    Introduction to MVC and Model Relations
    This was an introductory class to MVC and added a new folder to understand the relationships between Models

---
+++ Lecture10/WebUIMVC/WebUIMVC/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebUIMVC.MvcApplication" Language="C#" %>
---
+++ Lecture11/WebUIMVC/WebUIMVC/WebUIMVCContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+public class WebUIMVCContext : DbContext
+{
+    // You can add custom code to this file. Changes will not be overwritten.
+    // 
+    // If you want Entity Framework to drop and regenerate your database
+    // automatically whenever you change your model schema, please use data migrations.
+    // For more information refer to the documentation:
+    // http:// msdn.microsoft.com/en-us/data/jj591621.aspx
+
+    public WebUIMVCContext() : base("name=WebUIMVCContext")
+    {
+    }
+
+    public System.Data.Entity.DbSet<WebUIMVC.Models.Country> Countries { get; set; }
+
+    public System.Data.Entity.DbSet<WebUIMVC.Models.City> Cities { get; set; }
+}
