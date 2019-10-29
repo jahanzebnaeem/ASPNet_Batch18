be3ec9cb1ba22dea35aad403093707620480ee53
Jahanzeb Naeem <jahanzeb.naeem@hotmail.com>
2019-10-29 16:58:44 +0500

    Lecture 14
    The lecture is related to controllers.

---
+++ Lecture14/MVCControl/MVCControl/Controllers/TestController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCControl.Controllers
+{
+    public class TestController : Controller
+    {
+        // GET: Test
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Test Page";
+            ViewBag.Message = "This is the test page";
+            return View();
+        }
+    }
+}
---
+++ Lecture14/MVCControl/MVCControl/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MVCControl.Startup))]
+namespace MVCControl
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
